e3c54bdbfc0d0f4943853d6139b3d8403b908c28
atheeq2003 <atheequrrahaman8@gmail.com>
2022-05-19 00:06:50 +0530

    Initial Commit - Problem 1108

---
+++ P_1108.cpp
@@ -0,0 +1,31 @@
+//
+// Created by athee on 18-05-2022.
+//
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string str;
+        for(int i = 0; i < address.length(); i++)
+        {
+            if( address[i] == '.' )
+            {
+                str += "[.]";
+            }
+            else
+            {
+                str += address[i];
+            }
+        }
+        return str;
+    }
+};
+
+int main()
+{
+    Solution obj;
+    string address;
+    cin >> address;
+    cout << "Output is \n" + obj.defangIPaddr(address);
+}
